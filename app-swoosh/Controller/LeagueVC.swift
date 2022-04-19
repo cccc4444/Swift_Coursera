b70aa494eda94eaadcb7a0e5e196131fa7749421
cccc4444 <kushlyansky244@gmail.com>
2022-04-19 10:02:41 +0300

    Some refactroring

---
+++ app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,19 @@
+//
+//  LeagueVCViewController.swift
+//  app-swoosh
+//
+//  Created by Danylo Kushlianskyi on 19.04.2022.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
