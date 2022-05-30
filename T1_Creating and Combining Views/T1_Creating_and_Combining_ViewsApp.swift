66a1909befa9d913750fa2e5f50af105669a534f
cccc4444 <kushlyansky244@gmail.com>
2022-05-30 19:51:34 +0300

    Initial Commit

---
+++ T1_Creating and Combining Views/T1_Creating_and_Combining_ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  T1_Creating_and_Combining_ViewsApp.swift
+//  T1_Creating and Combining Views
+//
+//  Created by Danylo Kushlianskyi on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct T1_Creating_and_Combining_ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
